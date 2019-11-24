71dd8f39c63df8f7cee42ac84169620efc2598b1
Timur Khazamov <t1mmaas@skbkontur.ru>
2019-11-24 13:11:16 +0500

    Custom properties

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./index.module.scss";
 
 export function App() {
+  const [factor, setFactor] = useState(8);
   return (
-    <div className={`${styles.root} ${styles.bordered}`}>
-      <div className={`${styles.title} ${styles.bordered}`}>Hello world</div>
+    <div style={{ "--space-factor": factor + "px" } as React.CSSProperties}>
+      <input
+        type="range"
+        min="0"
+        max="16"
+        value={factor}
+        onChange={e => setFactor(Number(e.target.value))}
+      />
+      <div className={`${styles.root} ${styles.bordered}`}>
+        <div className={`${styles.title} ${styles.bordered}`}>Hello world</div>
+      </div>
     </div>
   );
 }
