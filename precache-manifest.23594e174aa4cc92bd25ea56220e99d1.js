dfd675509496e174c6bf09706df720ecfb765fd6
Timur Khazamov <t1mmaas@skbkontur.ru>
2019-11-24 13:12:59 +0500

    Updates

---
+++ precache-manifest.23594e174aa4cc92bd25ea56220e99d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a695dd24218b24b3722a224f332679",
+    "url": "/css-themable/index.html"
+  },
+  {
+    "revision": "b9ee52f856542f161975",
+    "url": "/css-themable/static/css/main.d1b433f5.chunk.css"
+  },
+  {
+    "revision": "8c75e41087e2ede193ec",
+    "url": "/css-themable/static/js/2.d77a51ba.chunk.js"
+  },
+  {
+    "revision": "b9ee52f856542f161975",
+    "url": "/css-themable/static/js/main.e142b5b9.chunk.js"
+  },
+  {
+    "revision": "ad87dbd0b10819e58b40",
+    "url": "/css-themable/static/js/runtime-main.70288b06.js"
+  }
+]);
